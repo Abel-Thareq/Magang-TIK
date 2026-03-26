0411903498318bf48b004a8cafcfaced030c776a
Abel <abel.thareq88@gmail.com>
2026-03-26 10:02:35 +0700

    update-12 (26 Mar)

--- Backend SiBMN/obj/Debug/net10.0/SiBMN.AssemblyInfo.cs
+++ Backend SiBMN/obj/Debug/net10.0/SiBMN.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SiBMN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61908bf8f6e817090c8fbc023ac6644df883b8cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc5025bef6a6556e78f684094cfa05078e5db7b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SiBMN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SiBMN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
